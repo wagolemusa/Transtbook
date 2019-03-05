7a79835f49165e5bc6c3e84dbb70697c3e52dec9
kisumutalent <homiemusa@gmail.com>
2019-03-05 12:10:16 +0300

    Input values

--- src/components/auth/Signin.js
+++ src/components/auth/Signin.js
@@ -4,16 +4,33 @@ import { MDBInput, MDBContainer } from 'mdbreact';
 
 class Signin extends Component {
 
+    state = {
+        username: '',
+        password: ''
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(this.state);
+    }
 
 
     render(){
         
         return(
             <MDBContainer>
+                <form onSubmit={this.handleSubmit} className="white">
+                     <MDBInput label="Username" type="text" id="username" onChange={this.handleChange} icon="user" />
+                    <MDBInput label="Password" type="password" id="password" onChange={this.handleChange} icon="key" />
+                    <button className="btn pink lighten-1 z-depth-0">Sign In</button>   
 
-                <MDBInput label="username" icon="user" />
-                <MDBInput label="passward" icon="key" />
-                
+                </form>
             </MDBContainer>
         )
 
--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,22 +1,45 @@
 import React, { Component } from 'react';
 
-import { MDBInput, MDBContainer } from "mdbreact";
+import { MDBInput, MDBContainer} from "mdbreact";
 
 class Signup extends Component {
 
+    state = {
+        first_name: '',
+        last_name: '',
+        username: '',
+        phone: '',
+        email: '',
+        password: '',
+        confirm_password:''
+
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(this.state);
+    }
 
 
     render(){
         return (
             <MDBContainer>
-                <MDBInput label="First Name" type="text"  icon="signature" />
-                <MDBInput label="Last Name" type="text"  icon="signature" />
-                <MDBInput label="Phone Number" type="number"  icon="phone-volume" />
-                <MDBInput label="Username" type="text"  icon="user" />
-                <MDBInput label="Email" type="email" icon="at" />
-                <MDBInput label="Password" type="password" icon="key" />
-                <MDBInput label="Re-password" type="password" icon="key" />
-
+                <form onSubmit={this.handleSubmit} className="white">
+                    <MDBInput label="First Name" type="text" id="first_name" onChange={this.handleChange} icon="signature" />
+                    <MDBInput label="Last Name" type="text" id="last_name" onChange={this.handleChange} icon="signature" />
+                    <MDBInput label="Phone Number" type="number" id="phone" onChange={this.handleChange}  icon="phone-volume" />
+                    <MDBInput label="Username" type="text" id="username" onChange={this.handleChange} icon="user" />
+                    <MDBInput label="Email" type="email" id="email" onChange={this.handleChange} icon="at" />
+                    <MDBInput label="Password" type="password" id="password" onChange={this.handleChange} icon="key" />
+                    <MDBInput label="Confirm password" type="password" id="confirm_password" onChange={this.handleChange} icon="key" />
+                    <button className="btn pink lighten-1 z-depth-0">Sign Up</button>   
+                 </form>
 
             </MDBContainer>
         )
